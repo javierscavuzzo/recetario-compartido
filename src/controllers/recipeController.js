177f0b3ca1d75db9b9a6cf46172cd1c1463bd646
Javier Scavuzzo <javierscavuzzo17@gmail.com>
2025-05-23 15:24:08 +0200

    Implementados endpoints CRUD de recetas

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -26,6 +26,55 @@ exports.getRecipes = async (req, res) => {
         const recipes = await Recipe.find().populate('createdBy', 'name email');
         res.json(recipes);
     } catch (error) {
-        res.status(500).json({ eror: error.message });
+        res.status(500).json({ error: error.message });
     }
+};
+
+// Obtener una receta por ID
+const getRecipeById = async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id);
+        if (!recipe) {
+            return res.status(404).json({ message: 'Receta no encontrada' });
+        }
+        res.status(200).json(recipe);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al obtener la receta' });
+    }
+};
+
+// Actualizar receta
+const updateRecipe = async (req, res) => {
+    try {
+        const recipe = await Recipe.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+        });
+        if (!recipe) {
+            return res.status(404).json({ message: 'Receta no encontrada' });
+        }
+        res.status(200).json(recipe);
+    } catch (error) {
+        res.status(500).json({ message: 'Error al actualizar la receta' });
+    }
+};
+
+// Eliminar receta
+const deleteRecipe = async (req, res) => {
+    try {
+        const recipe = await Recipe.findByIdAndDelete(req.params.id);
+        if (!recipe) {
+            return res.status(404).json({ message: 'Receta no encontrada' });
+        }
+        res.status(200).json({ message: 'Receta eliminada correctamente' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error al eliminar la receta' });
+    }
+};
+
+module.exports = {
+    createRecipe,
+    getAllRecipes,
+    getRecipeById,
+    updateRecipe,
+    deleteRecipe,
 };
--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -2,8 +2,18 @@ const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipeController');
 const authMiddleware = require('../middleware/authMiddleware');
+const {
+    createRecipe,
+    getAllRecipes,
+    getRecipeById,
+    updateRecipe,
+    deleteRecipe,
+} = require('../controllers/recipeController');
 
 router.post('/', authMiddleware, recipeController.createRecipe);
 router.get('/', recipeController.getRecipes);
+outer.get('/:id', getRecipeById);
+router.put('/:id', updateRecipe);
+router.delete('/:id', deleteRecipe);
 
 module.exports = router;
